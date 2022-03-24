6c0bd3f79bc37b2ec16c74e3e40a9f88014b0bb7
singleview-co-kr <w9721066@gmail.com>
2022-03-24 16:45:13 +0900

    sign version 1.12.0

--- config/package.inc.php
+++ config/package.inc.php
@@ -6,5 +6,4 @@ define('_XE_LOCATION_', 'ko');
 define('_XE_LOCATION_SITE_', 'http://www.xpressengine.com/');
 define('_XE_DOWNLOAD_SERVER_', 'http://download.xpressengine.com/');
 define('_XE_SV_LOCATION_SITE_', 'http://singleview.co.kr/');
-// define('_XE_SV_DOWNLOAD_SERVER_', 'http://singleview.co.kr/appstore/');
-define('_XE_SV_DOWNLOAD_SERVER_', 'http://192.168.0.37/xe-core-sv/appstore');
+define('_XE_SV_DOWNLOAD_SERVER_', 'http://singleview.co.kr/appstore/');
