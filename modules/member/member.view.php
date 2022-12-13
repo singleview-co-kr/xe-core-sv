2316d64b2bdb85973bf3a50ef378023e620db7e0
singleview-co-kr <w9721066@gmail.com>
2022-12-13 10:52:05 +0900

    Commit to #22
    회원 정보 자세히 보기 화면에서 핸폰 번호 출력되도록 수정
    
    modified:   modules/member/member.view.php

--- modules/member/member.view.php
+++ modules/member/member.view.php
@@ -95,7 +95,7 @@ class memberView extends member
 		}
 
 		$site_module_info = Context::get('site_module_info');
-		$columnList = array('member_srl', 'user_id', 'email_address', 'user_name', 'nick_name', 'homepage', 'blog', 'birthday', 'regdate', 'last_login', 'extra_vars');
+		$columnList = array('member_srl', 'user_id', 'email_address', 'mobile', 'user_name', 'nick_name', 'homepage', 'blog', 'birthday', 'regdate', 'last_login', 'extra_vars');
 		$member_info = $oMemberModel->getMemberInfoByMemberSrl($member_srl, $site_module_info->site_srl, $columnList);
 		unset($member_info->password);
 		unset($member_info->email_id);
