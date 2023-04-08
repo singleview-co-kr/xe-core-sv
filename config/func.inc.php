6f14be6fc9ce0f4113ce8ce1c6351b00fda1ab5e
singleview-co-kr <w9721066@gmail.com>
2023-04-08 13:44:46 +0900

    Resolve #44
    modified:   config/func.inc.php

--- config/func.inc.php
+++ config/func.inc.php
@@ -1542,7 +1542,7 @@ function isCrawler($agent = NULL)
 		$agent = $_SERVER['HTTP_USER_AGENT'];
 	}
 
-	$check_agent = array('bot', 'spider', 'spyder', 'crawl', 'http://', 'google', 'yahoo', 'slurp', 'yeti', 'daum', 'teoma', 'fish', 'hanrss', 'facebook', 'yandex', 'infoseek', 'askjeeves', 'stackrambler');
+	$check_agent = array('bot', 'spider', 'spyder', 'crawl', 'http://', 'google', 'yahoo', 'slurp', 'yeti', 'daum', 'teoma', 'fish', 'hanrss', 'facebook', 'yandex', 'infoseek', 'askjeeves', 'stackrambler', 'python');
 	$check_ip = array(
 		/*'211.245.21.110-211.245.21.119' mixsh is closed */
 	);
